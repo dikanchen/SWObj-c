eef38589f9da37a5ca96db81a46e15499e5b894f
User <user@users-mac-mini-3.local>
2018-02-27 17:17:27 -0500

    Initial Commit

---
+++ SWObj-c/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SWObj-c
+//
+//  Created by User on 2/27/18.
+//  Copyright © 2018 dikan.chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SWObj-cTests/SWObj_cTests.m
@@ -0,0 +1,39 @@
+//
+//  SWObj_cTests.m
+//  SWObj-cTests
+//
+//  Created by User on 2/27/18.
+//  Copyright © 2018 dikan.chen. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SWObj_cTests : XCTestCase
+
+@end
+
+@implementation SWObj_cTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
